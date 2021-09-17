1bf3d5601b831f6c2ee0564ebfb8175d3ab15936
pulgueta_ <roariasaf@gmail.com>
2021-09-16 19:59:32 -0500

    pong

---
+++ PForm/DBForm/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace DBForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dbLoadBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbLoadBtn
+            // 
+            this.dbLoadBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dbLoadBtn.FlatAppearance.BorderSize = 10;
+            this.dbLoadBtn.Location = new System.Drawing.Point(40, 50);
+            this.dbLoadBtn.Name = "dbLoadBtn";
+            this.dbLoadBtn.Size = new System.Drawing.Size(108, 33);
+            this.dbLoadBtn.TabIndex = 0;
+            this.dbLoadBtn.Text = "Don\'t click me";
+            this.dbLoadBtn.UseVisualStyleBackColor = true;
+            this.dbLoadBtn.Click += new System.EventHandler(this.dbLoadBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 236);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dbLoadBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dbLoadBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ PForm/DBForm/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBForm {
+    public partial class Form1 : Form {
+        public SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBPract;Integrated Security=True");
+        SqlCommand cmd;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            try {
+                con.Open();
+                MessageBox.Show("Connection done.");
+                con.Close();
+            } catch {
+                MessageBox.Show("Connection couldn't be done.");
+                this.Close();
+            }
+        }
+
+        private void dbLoadBtn_Click(object sender, EventArgs e) {
+            con.Open();
+
+            string query = "Select * from Alumni";
+            cmd = new SqlCommand(query, con);
+
+            SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            dAdapter.Fill(ds);
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+
+            con.Close();
+        }
+    }
+}
